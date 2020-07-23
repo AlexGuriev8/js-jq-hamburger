8bb476196f59b0b00e2e286a4e7fdaa7ada6b9b2
Alex <gurievalex78@gmail.com>
2020-07-23 17:27:26 +0200

    complete

--- README.md
+++
@@ -1,1 +0,0 @@
-js-jq-hamburger
---
+++ main.js
@@ -0,0 +1,9 @@
+ $('.header-right > a').click(
+    function(){
+     $('.hamburger-menu').show('slow');
+});
+
+$('.close').click(
+    function(){
+     $('.hamburger-menu').hide('slow');
+});
